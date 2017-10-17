e138f22cc188356faf69efee271aa5a69cff6f5f
Alexander Kiskin <estel4ever@gmail.com>
2017-10-17 22:56:52 +1030

    Application list renovated

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,83 +3,92 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 var axios = require('axios');
 
+
+const Container = (props) =>
+<div className="main-panel">
+    <div className="content">
+        {props.children}
+    </div>
+</div>
+
 const LoadingScreen = () => 
-    <div className="main-panel">
-        <div className="content">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="card">
-                        <div className="header">
-                            <h1>Ваши приложения</h1>
-                        </div>
-                        <div className="content">
-                            <h2><i className="pe-7s-refresh-2"></i> Загрузка...</h2>
-                        </div>
-                    </div>
-                </div>
+<div className="container-fluid">
+    <div className="row">
+        <div className="card">
+            <div className="header">
+                <h1>Ваши приложения</h1>
+            </div>
+            <div className="content">
+                <h2><i className="pe-7s-refresh-2"></i> Загрузка...</h2>
             </div>
         </div>
     </div>
+</div>
+
 
 
 
 
 const DataScreen = (props) => 
-    <div className="main-panel">
-        <div className="content">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="card">
-                        <div className="header">
-                            <h1>Ваши приложения</h1>
-                        </div>
-                        <div className="content">
-                            {props.apps.map(app => <SingleApp app={app} key={app.Id} />)}
-                            <div>
-                                <button className="btn btn-default" onClick={props.refresh}>
-                                    <i className="fa fa-refresh" aria-hidden="true"></i>
-                                    Обновить
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="card">
-                        <div className="content">
-                            <i className="fa fa-info-circle" aria-hidden="true"></i>
-                            Можно использовать тонкий клиент вместо браузера. <a href="https://bitstroitelstvo.atlassian.net/wiki/spaces/365KB/pages/66191361/1">Перейти в базу знаний</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="card">
-                        <div className="content">
-                            <i className="fa fa-info-circle" aria-hidden="true"></i>
-                            Подключение, отключение и изменение параметров приложений производится в разделе <Link to="/tariffs">Тарифы</Link>.
-                        </div>
-                    </div>
+<div className="container-fluid">
+    <div className="row">
+        <div className="card">
+            <div className="header">
+                <h1>Ваши приложения</h1>
+            </div>
+            <div className="content">
+                {props.apps.map(app => <SingleApp app={app} key={app.Id} />)}
+                <div>
+                    <button className="btn btn-default" onClick={props.refresh}>
+                        <i className="fa fa-refresh" aria-hidden="true"></i>
+                        Обновить
+                    </button>
                 </div>
             </div>
         </div>
     </div>
+    <div className="row">
+        <div className="card">
+            <div className="content">
+                <i className="fa fa-info-circle" aria-hidden="true"></i>
+                Можно использовать тонкий клиент вместо браузера. <a href="https://bitstroitelstvo.atlassian.net/wiki/spaces/365KB/pages/66191361/1">Перейти в базу знаний</a>
+            </div>
+        </div>
+    </div>
+    <div className="row">
+        <div className="card">
+            <div className="content">
+                <i className="fa fa-info-circle" aria-hidden="true"></i>
+                Подключение, отключение и изменение параметров приложений производится в разделе <Link to="/tariffs">Тарифы</Link>.
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
 const SingleApp = (props) => 
-    <div className="row">
-        <div className="col-md-5">
-            {props.app.Name}
-            <small>
-                {" (" + props.app.ConfigName + " " + props.app.ConfigVersionName + ")"}
-            </small>
-        </div>
-        <div className="col-md-2">
-            {props.app.Status}
-        </div>
-        <div className="col-md-5">
-            <a href={props.app.URLWithCredentials} target="_blank">{props.app.URL}</a>
+<div className="panel panel-default">
+    <div className="panel-heading">{props.app.Name}</div>
+    <div className="panel-body">
+        <div className="row">
+            <div className="col-md-4 vertical-align">
+                {props.app.ConfigName + " версии " + props.app.ConfigVersionName}
+            </div>
+            <div className="col-md-2 vertical-align">
+                {props.app.Status}
+            </div>
+            <div className="col-md-4 vertical-align">
+                <a href={props.app.URLWithCredentials} target="_blank">{props.app.URL}</a>
+            </div>
+            <div className="col-md-2 vertical-align">
+                <a href={props.app.URLWithCredentials} target="_blank">
+                    {props.app.URL && <button className="btn btn-default btn-fill">Запустить</button>}
+                </a>
+            </div>
         </div>
     </div>
+</div>
 
 
 
@@ -104,7 +113,9 @@ class Homepage extends Component {
 
     render() {
         return(
-            this.state.isFetching ? <LoadingScreen /> : <DataScreen apps={this.state.applications} refresh={this.getApplications} />
+            <Container>
+                {this.state.isFetching ? <LoadingScreen /> : <DataScreen apps={this.state.applications} refresh={this.getApplications} />}
+            </Container>
         );
     }
 
