8bfe887714d2da31e224540f8da780bff52dec1d
Alexander Kiskin <estel4ever@gmail.com>
2017-11-03 22:11:56 +1030

    No tariffs sign

--- src/tariffs/MyTariffs.js
+++ src/tariffs/MyTariffs.js
@@ -4,43 +4,48 @@ import { Link } from 'react-router-dom'
 var axios = require('axios');
 
 const LoadingScreen = () => 
-    <div className="card">
-        <div className="header">
-            <h1>Подключенные тарифы</h1>
-        </div>
-        <div className="content">
-            <div className="text-center"><i className="fa fa-refresh fa-spin fa-3x fa-fw"></i></div>
-        </div>
+<div className="card">
+    <div className="header">
+        <h1>Подключенные тарифы</h1>
     </div>
+    <div className="content">
+        <div className="text-center"><i className="fa fa-refresh fa-spin fa-3x fa-fw"></i></div>
+    </div>
+</div>
 
 
 const DataScreen = (props) => 
-    <div className="card">
-        <div className="header">
-            <h1>Подключенные тарифы</h1>
-        </div>
-        <div className="content">
-            {props.tariffs.map((tariff,index) => <SingleTariff tariff={tariff} key={index} />)}
-        </div>
+<div className="card">
+    <div className="header">
+        <h1>Подключенные тарифы</h1>
     </div>
+    <div className="content">
+        { props.tariffs.length === 0 ? <NoTariffs /> :
+            props.tariffs.map((tariff,index) => <SingleTariff tariff={tariff} key={index} />)}
+    </div>
+</div>
+
+const NoTariffs = () =>
+<p>Еще не подключено ни одного тарифа. Выберите один доступных из тарифов для подключения.</p>
+
 
 const SingleTariff = (props) => 
-    <div className="row">
-        <div className="col-md-4">
-            {props.tariff.App.Name}
-        </div>
-        <div className="col-md-4">
-            {props.tariff.Tariff.Name}
-        </div>
-        <div className="col-md-2">
-            {props.tariff.Nusers}
-        </div>
-        <div className="col-md-2">
-            <Link to={{pathname: "/tariffs/edit", state: {tariff: props.tariff}}}>
-                <button className="btn btn-info">Изменить</button>
-            </Link>
-        </div>
+<div className="row">
+    <div className="col-md-4">
+        {props.tariff.App.Name}
     </div>
+    <div className="col-md-4">
+        {props.tariff.Tariff.Name}
+    </div>
+    <div className="col-md-2">
+        {props.tariff.Nusers}
+    </div>
+    <div className="col-md-2">
+        <Link to={{pathname: "/tariffs/edit", state: {tariff: props.tariff}}}>
+            <button className="btn btn-info">Изменить</button>
+        </Link>
+    </div>
+</div>
 
 
 class MyTariffs extends Component {
@@ -54,7 +59,7 @@ class MyTariffs extends Component {
       }
     
     componentDidMount() {
-        
+
         axios.get(this.props.basePrivateURL + '/mytariffs',
             {
                 auth: {
