e5cea824aa152080820436ebbc787e3ec0b6ea7e
Alexander Kiskin <estel4ever@gmail.com>
2017-11-07 17:07:12 +1030

    Current tariffs visuals

--- src/tariffs/MyTariffs.js
+++ src/tariffs/MyTariffs.js
@@ -31,21 +31,27 @@ const NoTariffs = () =>
 
 const SingleTariff = (props) => 
 <div className="panel panel-default">
-    <div className="panel-heading">{props.tariff.App.Name}</div>
+    <div className="panel-heading">
+        <strong>{props.tariff.App.Name} </strong>
+        {props.tariff.Blocked && <span className="text-danger"><i className="fa fa-ban" aria-hidden="true"></i> финансовая блокировка</span>}
+        {props.tariff.Canceled && <span className="text-danger"><i className="fa fa-ban" aria-hidden="true"></i> отключен</span>}
+        {(!props.tariff.Blocked && !props.tariff.Canceled) && <span className="text-success"><i className="fa fa-check" aria-hidden="true"></i> активный</span>}
+        {props.tariff.TestPeriodOn && <span className="text-warning"><i className="fa fa-exclamation" aria-hidden="true"></i>тестовый период до {new Date(props.tariff.TestPeriodEndDate).toLocaleString("ru", {year: "numeric", month: "short", day: "numeric", hour: undefined, minute: undefined, second: undefined})}</span>}
+    </div>
     <div className="panel-body">
         <div className="row">
             <div className="col-md-4">
-                {props.tariff.App.Name}
+                Тариф: {props.tariff.Tariff.Name}
             </div>
             <div className="col-md-4">
-                {props.tariff.Tariff.Name}
+                Одновременных подключений: {props.tariff.Nusers}
             </div>
             <div className="col-md-2">
-                {props.tariff.Nusers}
+                {props.tariff.Cost} руб. в {props.tariff.Tariff.Periodicity.toLowerCase()}                
             </div>
             <div className="col-md-2">
                 <Link to={{pathname: "/tariffs/edit", state: {tariff: props.tariff}}}>
-                    <button className="btn btn-info">Изменить</button>
+                    <button className="btn btn-warning" disabled>Изменить</button>
                 </Link>
             </div>
         </div>
@@ -77,6 +83,7 @@ class MyTariffs extends Component {
             if (response.status === 200) {
                 this.setState({
                     tariffs: response.data, 
+                    //tariffs: [],
                     isFetching: false
                 });
             }
