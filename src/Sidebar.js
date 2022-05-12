bdd77575dd03ee7b12b6bbea954f9e83174c075f
Немцов Александр <anemcov@gmail.com>
2022-05-12 20:43:41 +0300

    module refactoring

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
+import { Link, withRouter } from 'react-router-dom';
+// import { withRouter } from 'react-router';
 
 class NavLink extends Link {
     render() {
